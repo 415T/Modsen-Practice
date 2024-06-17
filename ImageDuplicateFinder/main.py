c0a3ab1c7e7035d3e5ce643a1971cc28f16bdd37
415T <86910545+415T@users.noreply.github.com>
2024-06-17 09:50:14 +0300

    The skeleton ImageDuplicateFinder class is made.

--- ImageDuplicateFinder/main.py
+++ ImageDuplicateFinder/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import os
+from PIL import Image
+import numpy as np
+import imagehash
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class ImageDuplicateFinder:
+    def __init__(self, supported_formats=('jpg', 'jpeg')):
+        self.supported_formats = supported_formats
+        self.images = []
+        self.image_paths = []
+        self.hashes = []
+        self.duplicates = {}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    finder = ImageDuplicateFinder()
